5eb6d2270351f559f87d5ec31e8ddf4b666036dc
Niall Lewin <Lewin91@hotmail.com>
2020-01-17 11:34:48 +0000

    Updates

---
+++ precache-manifest.bd21c36ee611b6ec585e8311dbcd7e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117c34444387245033d0d94b5d6d9593",
+    "url": "/app-le/index.html"
+  },
+  {
+    "revision": "ac26958e87e67c850767",
+    "url": "/app-le/static/css/main.b7ecc2b8.chunk.css"
+  },
+  {
+    "revision": "5d2a64ac6c66f0185ac5",
+    "url": "/app-le/static/js/2.ea3240f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app-le/static/js/2.ea3240f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac26958e87e67c850767",
+    "url": "/app-le/static/js/main.8f4ae037.chunk.js"
+  },
+  {
+    "revision": "4fd0f7a1aa4ec6ae08af",
+    "url": "/app-le/static/js/runtime-main.27e75d3e.js"
+  },
+  {
+    "revision": "3d3d9ef36ae706db896ec4564e94b023",
+    "url": "/app-le/static/media/Apple.3d3d9ef3.png"
+  }
+]);
