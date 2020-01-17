967b18bbb9f348d4e6f5e8be57a5618c765545ce
Niall Lewin <Lewin91@hotmail.com>
2020-01-17 11:39:30 +0000

    Initial check-in

---
+++ src/components/questions/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import './questions.scss'
+
+class Questions extends Component {
+    render() {
+        const { colour, day, soup, updateColour, updateDay, updateSoup, getResult } = this.props.props;
+
+        const testFunc = () => {
+            console.log(this.props.props.state);
+        }
+
+        return (
+            <>
+                <form>
+                    <h1>Pick your favourites:</h1>
+                    <section className="options">
+                        <div className="option">
+                            <select className="control dropdown" value={colour} onChange={updateColour}>
+                                <option value="0" defaultValue>(Colour)</option>
+                                <option value="Blorange">Blorange</option>
+                                <option value="Fled">Fled</option>
+                                <option value="Muurple">Muurple</option>
+                                <option value="Splank">Splank</option>
+                                <option value="Jellow">Jellow</option>
+                            </select>
+                            {this.props.props.state.errors.colour !== "" && <p>{this.props.props.state.errors.colour}</p>}
+                        </div>
+                        <div className="option">
+                            <select className="control dropdown" value={day} onChange={updateDay}>
+                                <option value="0" defaultValue>(Day)</option>
+                                <option value="Flensday">Flensday</option>
+                                <option value="Floopday">Floopday</option>
+                                <option value="Flangaday">Flangaday</option>
+                                <option value="Fluteday">Fluteday</option>
+                                <option value="Flinkday">Flinkday</option>
+                                <option value="Flynsday">Flynsday</option>
+                                <option value="Friday">Friday</option>
+                            </select>
+                            {this.props.props.state.errors.day !== "" && <p>{this.props.props.state.errors.day}</p>}
+                        </div>
+                        <div className="option">
+                            <select className="control dropdown" value={soup} onChange={updateSoup}>
+                                <option value="0" defaultValue>(Soup)</option>
+                                <option value="Apple">Apple</option>
+                                <option value="Apple">Apple</option>
+                                <option value="Apple">Apple</option>
+                                <option value="Apple">Apple</option>
+                            </select>
+                            {this.props.props.state.errors.soup !== "" && <p>{this.props.props.state.errors.soup}</p>}
+                        </div>
+                        <input className="control" type="submit" value="Calculate" onClick={getResult} />
+                    </section>
+                    {testFunc()}
+                </form>
+            </>
+        )
+    }
+};
+
+export default Questions;
